efa4ca2743959e4e6750204ecf3b1b4a04dd5b90
PaulAdam <paul.adrian2001@yahoo.com>
2023-10-03 21:33:58 +0300

    Small fix

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -16,7 +16,7 @@ const About = () => {
                 <Fade bottom>
                     <img src={Picture} alt='Logo' className='logo' />
                     <p className='about-paragraph'>
-                        I am Ruby on Rails Engineer with nearly two years of hands-on experience in developing robust web applications. I'm a Computer Science graduate from Babes-Bolyai University, currently pursuing a master's degree in Entrepreneurship and Business Administration. <br /> <br />
+                        I am a Ruby on Rails Engineer with nearly two years of hands-on experience in developing robust web applications. I'm a Computer Science graduate from Babes-Bolyai University, currently pursuing a master's degree in Entrepreneurship and Business Administration. <br /> <br />
                         Because I'm passionate about sports and programming, I bring that enthusiasm to work every day. I make sure to stay energized, communicate effectively, and I'm always up for any opportunity that helps me grow, both personally and professionally. <br /> <br />
                         I take my work seriously, but I also enjoy adding humor and positivity to the team. It makes the workplace a friendlier and more enjoyable environment.
                     </p>
